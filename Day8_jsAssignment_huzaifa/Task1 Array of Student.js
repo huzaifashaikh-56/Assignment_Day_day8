8a36176e30730ef08161adbccedf04043ec8bc4c
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-09 17:49:12 +0530

    Update Task1 Array of Student.js

--- Day8_jsAssignment_huzaifa/Task1 Array of Student.js
+++ Day8_jsAssignment_huzaifa/Task1 Array of Student.js
@@ -1,1 +1,26 @@
 
+let students = [
+  {
+    name: "Ali",
+    rollNumber: 101,
+    marks: 85
+  },
+  {
+    name: "Fatima",
+    rollNumber: 102,
+    marks: 92
+  },
+  {
+    name: "Rahul",
+    rollNumber: 103,
+    marks: 78
+  }
+];
+
+// Example: Display all student details
+students.forEach(student => {
+  console.log("Name:", student.name);
+  console.log("Roll Number:", student.rollNumber);
+  console.log("Marks:", student.marks);
+  console.log("-------------");
+});
